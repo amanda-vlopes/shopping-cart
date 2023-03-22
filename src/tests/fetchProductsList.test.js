cd162c6e8fe0fad2fb347d6b8fe0b575add03f72
amanda-vlopes <amandavitor@hotmail.com>
2023-03-22 16:45:46 -0300

    Requisito 1 finalizado

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,17 +5,23 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    expect(typeof fetchProductsList).toBe('function');
   });
 
-  it('fetch é chamado ao executar fetchProductsList', () => {
-
+  it('fetch é chamado ao executar fetchProductsList', async () => {
+    await fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalled();
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+    await fetchProductsList('computador');
+    const produto = 'computador';
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=';
+    expect(fetch).toHaveBeenCalledWith(`${url}${produto}`);
   });
 
-  // it('...', () => {
-  // });
+  it('retorno da função fetchProductsList é uma estrutura de dados', async () => {
+    const result = await fetchProductsList('computador');
+    expect(typeof result).toBe('object');
+  });
 });
