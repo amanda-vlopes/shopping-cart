668e870530722559589e1d587ab6dffc1e641065
amanda-vlopes <amandavitor@hotmail.com>
2023-03-22 17:05:42 -0300

    Requisito 2 finalizado

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,7 +2,16 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
-
+export const fetchProductsList = async (produto) => {
+  if (!produto) {
+    throw new Error('Termo de busca não informado');
+  }
+  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=';
+  try {
+    const response = await fetch(`${url}${produto}`);
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    return error.message;
+  }
 };
